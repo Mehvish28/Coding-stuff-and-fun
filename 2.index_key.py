55caa70f2c6850d74bc2419f9fb67ea6d26a59fe
Mehvish28 <76213644+Mehvish28@users.noreply.github.com>
2023-04-03 12:46:47 -0400

    Delete 2.index_key.py

--- 2.index_key.py
+++
@@ -1,9 +0,0 @@
-'''Given an array A[] of N integers and an index Key. Your task is to print the element present at
-index key in the array.'''
-
-
-size = int(input("enter the size: "))
-index = int(input("enter index key: "))
-
-n = list(map(int,input().strip().split()))[:size]  # a good way of stripping spaces, splitting numbers and mapping them to a list of array
-print(n[index])
